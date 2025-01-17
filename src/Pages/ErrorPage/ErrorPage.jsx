5c6420cc974568b148aad23ac2d703fc3bdb1028
MujahidEmon <mujahidulislam.official@gmail.com>
2025-01-17 22:02:13 +0600

    ready

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -3,11 +3,12 @@ import { Link, useRouteError } from "react-router-dom";
 const ErrorPage = () => {
   const error = useRouteError();
   return (
-    <div className="flex gap-5 items-center justify-center min-h-screen flex-col">
+    <div className="flex bg-[#f4f6f9] gap-5 items-center justify-center min-h-screen flex-col">
       <h1 className="font-bold text-4xl">Oops!</h1>
       <p className="text-3xl">Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i className="text-3xl font-semibold">{error.statusText || error.message}</i>
+      <p className="text-3xl font-semibold">
+        <span className="mr-2">{error.status}</span>
+        {error.statusText || error.message}
       </p>
       <Link to={'/'} className="btn bg-green-500 text-white font-semibold">Go To Homepage</Link>
     </div>
